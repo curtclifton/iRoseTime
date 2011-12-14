ffbe1545d620a9cf027cfee0eb1957e5f0563d96
Curt Clifton <curt.clifton@mac.com>
2011-12-13 21:03:48 -0800

    Adding About tab with acknowledgments.

---
+++ Classes/AboutViewController.h
@@ -0,0 +1,14 @@
+//
+//  AboutViewController.h
+//  iRoseTime
+//
+//  Created by Curtis Clifton on 12/13/2011.
+//  Copyright 2010-11 by Curtis Clifton
+//  Released under a BSD license
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
---
+++ Classes/AboutViewController.m
@@ -0,0 +1,52 @@
+//
+//  AboutViewController.m
+//  iRoseTime
+//
+//  Created by Curtis Clifton on 12/13/2011.
+//  Copyright 2010-11 by Curtis Clifton
+//  Released under a BSD license
+//
+
+#import "AboutViewController.h"
+
+@implementation AboutViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
